4d9b39067df7692426dc56e3d3212ad638249266
Faysal Al-Kharraz <faysalkharraz@gmail.com>
2021-01-22 20:07:20 +0000

    Lecture 153: I have added the lecture notes and solved the challenge given.

---
+++ DOM/Lecture_153/src/script.js
@@ -0,0 +1,7 @@
+/* Add the .huge class to <h1> */
+console.log("Current list of classes that belong to <h1> with id=title");
+document.querySelector("#title").classList;
+
+document.querySelector("#title").classList.add("huge");
+console.log("Current list of classes that belong to <h1> following the addition of the css class .huge");
+document.querySelector("#title").classList;
